c1bc5643f70aa54008fe79c8ce634c2357c4426c
jinweihuang20 <101879478+jinweihuang20@users.noreply.github.com>
2025-12-12 19:27:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',  // ← 新增這行
   plugins: [vue()],
   server: {
     allowedHosts: ['meshmap.gwtech.org', 'localhost'],
